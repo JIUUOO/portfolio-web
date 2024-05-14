7120dc27a90792ebffe4533f60cd1de10c00242e
JIUUOO <nninjiuuoo@gmail.com>
2024-05-14 11:10:39 +0900

    test: design change

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,8 +1,8 @@
 export default function Home(props) {
   return (
     <div className={props.currSlide === 0 ? "" : "hidden"}>
-      <div className="w-screen h-screen bg-red-100">
-        <div>안녕하세요</div>
+      <div className="flex w-screen h-screen bg-red-100">
+        <div className="">안녕하세요</div>
         <div>클릭하여 다음 슬라이드로 이동하세요</div>
       </div>
     </div>
--- src/pages/skills/index.jsx
+++ src/pages/skills/index.jsx
@@ -1,7 +1,7 @@
 export default function Skills(props) {
   return (
     <div className={props.currSlide === 1 ? "" : "hidden"}>
-      <div className="w-screen h-screen bg-blue-100">
+      <div className="flex w-screen h-screen bg-blue-100">
         <div>제 기술 스택이에요</div>
         <div>클릭하여 다음 슬라이드로 이동하세요</div>
       </div>
